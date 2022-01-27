d327aa2c9646cebf60b961c9bf79894fd914d26a
Joey Kelroy <joeyk@axosoft.com>
2022-01-27 08:32:49 -0700

    update .cs file

---
+++ example.cs
@@ -0,0 +1,7 @@
+private Mock<example1> example1;
+private Mock<example2> example2;
+private Mock<example3> example3;
+private Mock<example4> example4;
+private Mock<example5> example5;
+private Mock<example6> example6;
+private Mock<example7> example7;
